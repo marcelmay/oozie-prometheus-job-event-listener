d5baf5e9d310dfa4f68e0d4dcced7196be6cdf3e
Marcel May <marcel.may.de@gmail.com>
2019-01-31 22:52:11 +0100

    Changed metric registration for listener lifecycle handling
    - Refactored label handling
    - Changed collector registration to oozie listener lifecycle init/destroy
    - Extended unit tests for all events and init/destroy

--- src/main/java/de/m3y/oozie/prometheus/PrometheusJobEventListener.java
+++ src/main/java/de/m3y/oozie/prometheus/PrometheusJobEventListener.java
@@ -2,6 +2,7 @@ package de.m3y.oozie.prometheus;
 
 import java.util.Date;
 
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import org.apache.hadoop.conf.Configuration;
@@ -13,9 +14,12 @@ import org.apache.oozie.util.XLog;
 /**
  * Provides Oozie job metrics for Prometheus:
  * <ul>
- * <li>oozie_workflow_job_duration_seconds : Duration of completed jobs, including job name and changed state. Can be used for alerting on lame jobs.</li>
- * <li>oozie_workflow_job_state_time_seconds : Timestamp of completed job state changes, including job name and changed state. Can be used for alerting on last successful run</li>
- * <li>oozie_workflow_job_total : Count of completed job state changes, including job name and changed state. Can be used to trigger alert on job failure rate.</li>
+ * <li>oozie_workflow_job_duration_seconds : Duration of completed jobs, including job name and changed state.
+ * Can be used for alerting on lame jobs.</li>
+ * <li>oozie_workflow_job_state_time_seconds : Timestamp of completed job state changes, including job name and
+ * changed state. Can be used for alerting on last successful run</li>
+ * <li>oozie_workflow_job_total : Count of completed job state changes, including job name and changed state.
+ * Can be used to trigger alert on job failure rate.</li>
  * </ul>
  */
 public class PrometheusJobEventListener extends JobEventListener {
@@ -25,114 +29,117 @@ public class PrometheusJobEventListener extends JobEventListener {
     private static final String LABEL_APP_NAME = "app_name";
     private static final String LABEL_STATUS = "status";
 
-    private static final Gauge WORKFLOW_JOB_DURATION = Gauge.build()
+    private final Gauge workflowJobDuration = Gauge.build()
             .name("oozie_workflow_job_duration_seconds")
             .help("Duration of completed jobs")
             .labelNames(LABEL_JOB_TYPE, LABEL_APP_NAME, LABEL_STATUS)
-            .register();
-    private static final Gauge WORKFLOW_JOB_EVENT = Gauge.build()
+            .create();
+    private final Gauge workflowJobEventTimestamp = Gauge.build()
             .name("oozie_workflow_job_state_time_seconds")
             .help("Timestamp of completed job state changes")
             .labelNames(LABEL_JOB_TYPE, LABEL_APP_NAME, LABEL_STATUS)
-            .register();
-    private static final Counter WORKFLOW_JOB_EVENT_TOTAL = Counter.build()
+            .create();
+    private final Counter workflowJobTotal = Counter.build()
             .name("oozie_workflow_job_total")
             .help("Count of completed job state changes")
             .labelNames(LABEL_JOB_TYPE, LABEL_APP_NAME, LABEL_STATUS)
-            .register();
+            .create();
 
     @Override
     public void init(Configuration configuration) {
         LOG.info("Initializing Prometheus Job Event Listener");
+        workflowJobDuration.register();
+        workflowJobEventTimestamp.register();
+        workflowJobTotal.register();
     }
 
     @Override
     public void destroy() {
         LOG.info("Destroying Prometheus Job Event Listener");
+        CollectorRegistry.defaultRegistry.unregister(workflowJobDuration);
+        CollectorRegistry.defaultRegistry.unregister(workflowJobEventTimestamp);
+        CollectorRegistry.defaultRegistry.unregister(workflowJobTotal);
     }
 
     @Override
     public void onWorkflowJobEvent(WorkflowJobEvent workflowJobEvent) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug(workflowJobEvent);
+            LOG.debug("Received workflow job event {}", workflowJobEvent);
         }
 
         final String statusName = workflowJobEvent.getStatus().name();
-        updateDuration(workflowJobEvent, statusName);
-        updateStatusEvent(workflowJobEvent, statusName);
+        final String[] labels = getLabels(workflowJobEvent, statusName);
+        updateDuration(workflowJobEvent, labels);
+        updateStatusEvent(labels);
     }
 
 
     @Override
     public void onWorkflowActionEvent(WorkflowActionEvent workflowActionEvent) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug(workflowActionEvent);
+            LOG.debug("Received workflow action event {}", workflowActionEvent);
         }
 
         final String statusName = workflowActionEvent.getStatus().name();
-        updateDuration(workflowActionEvent, statusName);
-        updateStatusEvent(workflowActionEvent, statusName);
+        final String[] labels = getLabels(workflowActionEvent, statusName);
+        updateDuration(workflowActionEvent, labels);
+        updateStatusEvent(labels);
     }
 
     @Override
     public void onCoordinatorJobEvent(CoordinatorJobEvent coordinatorJobEvent) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug(coordinatorJobEvent);
+            LOG.debug("Received coordinator job event {}", coordinatorJobEvent);
         }
 
         final String statusName = coordinatorJobEvent.getStatus().name();
-        updateDuration(coordinatorJobEvent, statusName);
-        updateStatusEvent(coordinatorJobEvent, statusName);
+        final String[] labels = getLabels(coordinatorJobEvent, statusName);
+        updateDuration(coordinatorJobEvent, labels);
+        updateStatusEvent(labels);
     }
 
     @Override
     public void onCoordinatorActionEvent(CoordinatorActionEvent coordinatorActionEvent) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug(coordinatorActionEvent);
+            LOG.debug("Received coordinator action event {}", coordinatorActionEvent);
         }
 
         final String statusName = coordinatorActionEvent.getStatus().name();
-        updateDuration(coordinatorActionEvent, statusName);
-        updateStatusEvent(coordinatorActionEvent, statusName);
+        final String[] labels = getLabels(coordinatorActionEvent, statusName);
+        updateDuration(coordinatorActionEvent, labels);
+        updateStatusEvent(labels);
     }
 
     @Override
     public void onBundleJobEvent(BundleJobEvent bundleJobEvent) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug(bundleJobEvent);
+            LOG.debug("Received bundle job event {}", bundleJobEvent);
         }
 
         final String statusName = bundleJobEvent.getStatus().name();
-
-        updateDuration(bundleJobEvent, statusName);
-        updateStatusEvent(bundleJobEvent, statusName);
+        final String[] labels = getLabels(bundleJobEvent, statusName);
+        updateDuration(bundleJobEvent, labels);
+        updateStatusEvent(labels);
     }
 
-    private void updateStatusEvent(JobEvent event, String status) {
-        final String appTypeName = event.getAppType().name();
-        final String appName = event.getAppName();
-        WORKFLOW_JOB_EVENT.labels(
-                appTypeName,
-                appName,
-                status
-        ).setToCurrentTime();
-        WORKFLOW_JOB_EVENT_TOTAL.labels(
-                appTypeName,
-                appName,
-                status
-        ).inc();
+    private String[] getLabels(JobEvent jobEvent, String status) {
+        return new String[]{jobEvent.getAppType().name(), jobEvent.getAppName(), status};
     }
 
-    private void updateDuration(JobEvent jobEvent, String status) {
+    private void updateStatusEvent(String[] labels) {
+        workflowJobEventTimestamp.labels(labels)
+                .setToCurrentTime();
+        workflowJobTotal.labels(labels)
+                .inc();
+    }
+
+    private void updateDuration(JobEvent jobEvent, String[] labels) {
         final Date endTime = jobEvent.getEndTime();
         final Date startTime = jobEvent.getStartTime();
         if (null != endTime && null != startTime) {
             final long duration = endTime.getTime() - startTime.getTime();
-            WORKFLOW_JOB_DURATION.labels(
-                    jobEvent.getAppType().name(),
-                    jobEvent.getAppName(),
-                    status
-            ).set(duration / 1000d /* Convert ms to seconds */);
+            workflowJobDuration.labels(labels)
+                    .set(duration / 1000d /* Convert ms to seconds */);
         }
     }
 }
--- src/test/java/de/m3y/oozie/prometheus/PrometheusJobEventListenerTest.java
+++ src/test/java/de/m3y/oozie/prometheus/PrometheusJobEventListenerTest.java
@@ -5,8 +5,13 @@ import java.util.Date;
 
 import io.prometheus.client.Collector.MetricFamilySamples;
 import io.prometheus.client.CollectorRegistry;
+import org.apache.oozie.AppType;
+import org.apache.oozie.client.CoordinatorAction;
 import org.apache.oozie.client.Job;
-import org.apache.oozie.event.BundleJobEvent;
+import org.apache.oozie.client.WorkflowAction;
+import org.apache.oozie.client.WorkflowJob;
+import org.apache.oozie.event.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static de.m3y.prometheus.assertj.MetricFamilySamplesAssert.assertThat;
@@ -15,11 +20,72 @@ import static de.m3y.prometheus.assertj.MetricFamilySamplesUtils.getMetricFamily
 import static org.assertj.core.api.Assertions.within;
 
 public class PrometheusJobEventListenerTest {
+    @Before
+    public void setUp() {
+        CollectorRegistry.defaultRegistry.clear();
+    }
+
+    @Test
+    public void testOnWorkflowJobEvent() {
+        PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
+
+        long start0 = System.currentTimeMillis();
+        long end0 = start0 + 100L;
+        final WorkflowJobEvent jobEvent = new WorkflowJobEvent("id#0", "parentId",
+                WorkflowJob.Status.SUCCEEDED, "user #0", "app #0",
+                new Date(start0), new Date(end0));
+        prometheusJobEventListener.onWorkflowJobEvent(jobEvent);
+
+        validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+    }
+
+    @Test
+    public void testOnWorkflowActionEvent() {
+        PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
+
+        long start0 = System.currentTimeMillis();
+        long end0 = start0 + 100L;
+        final WorkflowActionEvent jobEvent = new WorkflowActionEvent("id#0", "parentId",
+                WorkflowAction.Status.START_RETRY, "user #0", "app #0",
+                new Date(start0), new Date(end0));
+        prometheusJobEventListener.onWorkflowActionEvent(jobEvent);
+
+        validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+    }
+
+    @Test
+    public void testOnCoordinatorJobEvent() {
+        PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
+
+        long start0 = System.currentTimeMillis();
+        long end0 = start0 + 100L;
+        final CoordinatorJobEvent jobEvent = new CoordinatorJobEvent("id#0", "parentId",
+                Job.Status.FAILED, "user #0", "app #0",
+                new Date(start0), new Date(end0));
+        prometheusJobEventListener.onCoordinatorJobEvent(jobEvent);
+
+        validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+    }
+
+    @Test
+    public void testOnCoordinatorActionEvent() {
+        PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
+
+        long start0 = System.currentTimeMillis();
+        long end0 = start0 + 100L;
+        final CoordinatorActionEvent jobEvent = new CoordinatorActionEvent("id#0", "parentId",
+                CoordinatorAction.Status.READY, "user #0", "app #0",
+                null, new Date(start0),
+                "missing deps");
+        jobEvent.setEndTime(new Date(end0));
+        prometheusJobEventListener.onCoordinatorActionEvent(jobEvent);
+
+        validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+    }
+
     @Test
     public void testOnBundleJobEvent() {
-        CollectorRegistry.defaultRegistry.clear();
-
-        PrometheusJobEventListener prometheusJobEventListener = new PrometheusJobEventListener();
+        PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
 
         long start0 = System.currentTimeMillis();
         long end0 = start0 + 100L;
@@ -27,12 +93,39 @@ public class PrometheusJobEventListenerTest {
                 new Date(start0), new Date(end0));
         prometheusJobEventListener.onBundleJobEvent(jobEvent);
 
+        validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+    }
+
+    @Test
+    public void testInitAndDestroy() {
+        for(int i=0;i<2;i++) {
+            PrometheusJobEventListener prometheusJobEventListener = createAndInitPrometheusJobEventListener();
+
+            long start0 = System.currentTimeMillis();
+            long end0 = start0 + 100L;
+            final BundleJobEvent jobEvent = new BundleJobEvent("id#0", Job.Status.PREP, "user #0", "app #0",
+                    new Date(start0), new Date(end0));
+            prometheusJobEventListener.onBundleJobEvent(jobEvent);
+
+            validateJobEvent(start0, end0, jobEvent.getAppType(), jobEvent.getAppName(), jobEvent.getStatus().name());
+
+            prometheusJobEventListener.destroy();
+        }
+    }
+
+    private PrometheusJobEventListener createAndInitPrometheusJobEventListener() {
+        PrometheusJobEventListener prometheusJobEventListener = new PrometheusJobEventListener();
+        prometheusJobEventListener.init(null);
+        return prometheusJobEventListener;
+    }
+
+    private void validateJobEvent(long start0, long end0, AppType appType, String appName, String name) {
         MetricFamilySamples mfsWorkflowJobTotal = getMetricFamilySamples("oozie_workflow_job_total");
         assertThat(mfsWorkflowJobTotal)
                 .hasTypeOfCounter()
                 .hasSampleLabelNames("job_type", "app_name", "status")
                 .hasSampleValue(
-                        labelValues(jobEvent.getAppType().name(), jobEvent.getAppName(), jobEvent.getStatus().name()),
+                        labelValues(appType.name(), appName, name),
                         1d /* one count */
                 );
 
@@ -41,7 +134,7 @@ public class PrometheusJobEventListenerTest {
                 .hasTypeOfGauge()
                 .hasSampleLabelNames("job_type", "app_name", "status")
                 .hasSampleValue(
-                        labelValues(jobEvent.getAppType().name(), jobEvent.getAppName(), jobEvent.getStatus().name()),
+                        labelValues(appType.name(), appName, name),
                         (end0 - start0) / 1000d // ms to seconds
                 );
 
@@ -51,7 +144,7 @@ public class PrometheusJobEventListenerTest {
                 .hasAnySamples()
                 .hasSampleLabelNames("job_type", "app_name", "status")
                 .hasSampleValue(
-                        labelValues(jobEvent.getAppType().name(), jobEvent.getAppName(), jobEvent.getStatus().name()),
+                        labelValues(appType.name(), appName, name),
                         da -> da.isEqualTo(start0 / 1000d, within(1d))
                 );
     }
